3c8d52c92e33296546ac68b912a2e13f9b64dd65
L0laapk3 <L0laapk3@gmail.com>
2021-06-08 21:01:33 +0200

    added output functions to import

--- wasm.js
+++ wasm.js
@@ -5,28 +5,38 @@ const contents = fs.readFileSync(process.argv[2], {encoding: 'base64'});
 const unique = new Date().getTime();
 
 fs.writeFileSync("bot.js", `\
-x = 0;
+memory.spirits = Object.values(spirits);
 if (memory.wasm_cache != ${unique}) {
 	buff = Buffer.from("${contents}", "base64");
-
 	arr = new Uint8Array(buff.length);
 	for (let i = 0; i < buff.length; ++i)
 		arr[i] = buff[i];
-
-	importObject = { 	
-		imports: {
-			imported_func: function(u) {
-				x = u;
-			}
-		}
+	importObject = {
+        imports: {
+            energize: function(from, to) {
+                memory.spirits[from].energize(spirits[to]);
+            },
+            move: function(from, x, y) {
+                memory.spirits[from].move([x, y]);
+            },
+            merge: function(from, to) {
+                memory.spirits[from].merge(spirits[to]);
+            },
+            divide: function(from) {
+                memory.spirits[from].divide();
+            },
+            jump: function(from, x, y) {
+                memory.spirits[from].jump([x, y]);
+            },
+        }
 	};
 
 	wasm = new WebAssembly.Module(arr);
 	inst = new WebAssembly.Instance(wasm, importObject);
-	memory.wasm_run = inst.exports.exported_func;
+	memory.wasm_tick = inst.exports.tick;
 	memory.wasm_cache = ${unique};
 	console.log("compiled new wasm script");
 }
-memory.wasm_run();
-console.log(x);
+
+memory.wasm_tick();
 `);
