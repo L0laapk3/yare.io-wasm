5ae99e6da5a46763a05fe7eef206b6521720d283
L0laapk3 <L0laapk3@gmail.com>
2021-06-12 11:15:13 +0200

    panel resize

--- codeUpdate.user.js
+++ codeUpdate.user.js
@@ -14,23 +14,32 @@
 (function() {
     'use strict';
 
-    const UPDATE_ONLY_ACTIVE_TABS = true;
-	
+    const UPDATE_ONLY_ACTIVE_TABS = false;
+
 
     let focus = true;
     window.onblur = _ => focus = false;
     window.onfocus = _ => focus = true;
 
-    setInterval(async _ => {
-        if (focus || !UPDATE_ONLY_ACTIVE_TABS) {
-            const req = fetch("http://127.0.0.1:8194");
-            req.catch(_ => {}).then(async r => {
-                if (!r)
-                    return;
-                editor.setValue(await (r.text()));
-                update_code();
-            });
-        }
-    }, 2000);
+    function checkPanel() {
+        const panel = document.getElementById("panel");
+        if (!panel || !window.editor_container_el)
+            return setTimeout(checkPanel, 50);
+        panel.style.width = "20px";
+        window.editor_container_el.style.width = new_panel_width + 'px';
+        window.editor.resize();
 
+        setInterval(async _ => {
+            if (focus || !UPDATE_ONLY_ACTIVE_TABS) {
+                const req = fetch("http://127.0.0.1:8194");
+                req.catch(_ => {}).then(async r => {
+                    if (!r)
+                        return;
+                    editor.setValue(await (r.text()));
+                    update_code();
+                });
+            }
+        }, 2000);
+    }
+    checkPanel();
 })();
